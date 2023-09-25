d8bebefa96cd77be95985fcffeaa5db76ff441e3
Şeyma <seymacakmakci@192.168.1.104>
2023-09-25 20:05:41 +0300

    first commit

---
+++ HomeWorks/W2_1/Entities/Playlist.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace W2_1.Entities
+{
+    internal class Playlist
+    {
+        public Guid Id { get; set; }
+        private List<Song> Songs { get; set; }
+        private Random random;
+
+        public Playlist(Song firstSong)
+        {
+            Songs = new List<Song>();
+            random = new Random();
+            AddSong(firstSong);
+        }
+
+        public void AddSong(Song song)
+        {
+            if (song != null)
+                Songs.Add(song);
+        }
+
+        public string GetSongs()
+        {
+            return string.Join("\n", Songs.Select(songs => $"{songs.Title} - {songs.Composer}"));
+        }
+
+        public void ShuffleSongs()
+        {
+            int songCount = Songs.Count;
+
+            while (songCount > 0)
+            {
+                songCount--;
+                Song song = Songs[songCount];
+
+                int randomIndex = random.Next(songCount);
+                Songs[songCount] = Songs[randomIndex];
+                Songs[randomIndex] = song;
+            }
+        }
+    }
+}
---
+++ HomeWorks/W2_1/Services/NotepadService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace W2_1.Services
+{
+    internal class NotepadService
+    {
+
+        public void WriteToNotepad(string data)
+        {
+            Console.WriteLine("Data Successfully Saved to Notepad!");
+        }
+
+    }
+}
+
+
